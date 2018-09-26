be170bea98940206075b5d888e41b1b516adf907
Robert DeFelice <trombonedrummer@gmail.com>
2018-09-25 22:02:42 -0400

    Added Title Screen

---
+++ EGD Randomized Score/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    /*Variables*/
+    public int sceneName;
+    public GameObject playButton;
+    public GameObject quitButton;
+    public GameObject controlPanel;
+    bool active = false;
+
+    /*Functions*/
+    public void Controls()
+    {
+        if (active == false)
+        {
+            controlPanel.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            controlPanel.SetActive(false);
+            active = false;
+        }
+    }
+
+    public void Play ()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+}
