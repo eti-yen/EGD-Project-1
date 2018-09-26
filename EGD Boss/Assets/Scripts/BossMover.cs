c9ea6f30e5e0271a8f22f833f9bb946aa79b42e4
eti-yen <etiennem@sbcglobal.net>
2018-09-26 11:34:36 -0400

    Fixed boss bullets

---
+++ EGD Boss/Assets/Scripts/BossMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMover : MonoBehaviour
+{
+	public float speed;
+
+	void Start()
+	{
+		GetComponent<Rigidbody>().velocity = transform.right * speed;
+	}
+}
